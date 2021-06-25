a406d2cb49ae7f5f46a3d6995e8fa485dbec24b2
alex <alexandru.cristea@schubwerk.com>
2021-06-25 23:47:42 +0200

    some fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
       <b-carousel
           id="carousel-1"
-          class="my-carousel"
+          fade
           v-model="slide"
           controls
           indicators
@@ -10,25 +10,48 @@
           img-width="1024"
           img-height="480"
           style="text-shadow: 1px 1px 2px #333;"
-          @sliding-start="onSlideStart"
-          @sliding-end="onSlideEnd"
       >
           <!-- Text slides with image -->
           <b-carousel-slide class="my-carousel-slide"
               caption="First slide"
-              text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-              img-src="https://images.unsplash.com/photo-1520532240459-395dc4a823b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80"
-          ></b-carousel-slide>
+              text="Nulla vitae elit libero, a pharetra augue mollis interdum.">
+              <template #img>
+                  <img
+                      class="d-block img-fluid w-100"
+                      width="1024"
+                      height="480"
+                      src="../assets/lemons.jpeg"
+                      alt="image slot"
+                  >
+              </template>
+          </b-carousel-slide>
 
           <!-- Slides with custom text -->
-          <b-carousel-slide class="my-carousel-slide"
-              img-src="https://images.unsplash.com/photo-1530610476181-d83430b64dcd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80">
+          <b-carousel-slide class="my-carousel-slide">
+              <template #img>
+                  <img
+                      class="d-block img-fluid w-100"
+                      width="1024"
+                      height="480"
+                      src="../assets/pages.jpeg"
+                      alt="image slot"
+                  >
+              </template>
               <h1>Hello world!</h1>
           </b-carousel-slide>
 
           <!-- Slides with image only -->
-          <b-carousel-slide class="my-carousel-slide"
-              img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+          <b-carousel-slide class="my-carousel-slide">
+              <template #img>
+                  <img
+                      class="d-block img-fluid w-100"
+                      width="1024"
+                      height="480"
+                      src="../assets/salad.jpeg"
+                      alt="image slot"
+                  >
+              </template>
+          </b-carousel-slide>
 
           <!-- Slides with img slot -->
           <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
@@ -38,7 +61,7 @@
                       class="d-block img-fluid w-100"
                       width="1024"
                       height="480"
-                      src="https://picsum.photos/1024/480/?image=55"
+                      src="../assets/lemons.jpeg"
                       alt="image slot"
                   >
               </template>
@@ -56,17 +79,32 @@
 </template>
 
 <script>
+import '../assets/lemons.jpeg'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    },
+    data() {
+        return {
+            slide: 0,
+            sliding: null
+        }
+    },
+    methods: {
+        onSlideStart(slide) {
+            this.sliding = true
+        },
+        onSlideEnd(slide) {
+            this.sliding = false
+        }
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.my-carousel{
+#carousel-1{
     z-index: -99;
     width: 80%;
     margin: auto;
